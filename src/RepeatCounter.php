aad331976e6057c8164373a8b5206b4e1b1f1b62
Julian Stewart <stewart.julian@gmail.com>
2015-08-14 14:02:36 -0700

    pass null null test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,7 +3,10 @@
     {
         function countRepeats($input_one, $input_two)
         {
-
+            if ($input_one == (string)"" && $input_two == (string)"") {
+                $output = "error";
+            }
+            return $output;
         }
     }
 ?>
