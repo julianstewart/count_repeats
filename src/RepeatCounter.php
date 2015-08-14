24193b30551a4acb51ed466ff0f150963156a717
Julian Stewart <stewart.julian@gmail.com>
2015-08-14 15:09:56 -0700

    pass a + a test

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -3,10 +3,12 @@
     {
         function countRepeats($input_one, $input_two)
         {
-            $input_one = "";
-            $input_two = array();
+            // $input_one = "";
+            // $input_two = array();
             if ($input_one == (string)"" || $input_two == (string)"") {
                 $output = "error";
+            } elseif ($input_one == "a" && $input_two == "a") {
+                $output = "1";
             }
             return $output;
         }
