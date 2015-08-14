80468e9642d1296efe5adacba833d560d0c30cca
Julian Stewart <stewart.julian@gmail.com>
2015-08-14 15:12:08 -0700

    fail a + aa test

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -47,7 +47,6 @@
                 //assert
                 $this->assertEquals("error", $result);
             }
-
             // test for when input_one has a value of "a"
             // and input_two has a value of "a"
             function test_countRepeats_aA()
@@ -62,6 +61,22 @@
 
                 //assert
                 $this->assertEquals("1", $result);
+            }
+
+            // test for when input_one has a value of "a"
+            // and input_two has a value of "a a"
+            function test_countRepeats_aAA()
+            {
+                //arrange
+                $test_RepeatCounter = new RepeatCounter;
+                $input_one = "a";
+                $input_two = "a a";
+
+                //act
+                $result = $test_RepeatCounter->countRepeats($input_one, $input_two);
+
+                //assert
+                $this->assertEquals("2", $result);
             }
         }
     }
